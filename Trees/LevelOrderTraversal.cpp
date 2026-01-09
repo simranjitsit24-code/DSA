6a1d46f3fc4ae019bf1213c7a6798a4f9c8c2bfb
simranjitsit24-code <simranjits.it.24@nitj.ac.in>
2026-01-09 05:20:53 +0000

    Level Order Traversal

--- Trees/LevelOrderTraversal.cpp
+++ Trees/LevelOrderTraversal.cpp
@@ -10,6 +10,8 @@ public:
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
+
+
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) 
