f3c46350daca89230262d1245de0942aa1b21915
simranjitsit24-code <simranjits.it.24@nitj.ac.in>
2026-01-27 17:26:37 +0000

    Check if some element in array has its double

---
+++ Vectors/Check_If_its_Double_Exist.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        sort(arr.begin(),arr.end());
+        if (arr.empty()) return false;
+        int n = arr.size();
+        for (int i=0;i<n;i++)
+        {
+            int j = i+1;
+            int k= n-1;
+            while (j<=k)
+            {
+               int mid = j+(k-j)/2;
+               if(arr[mid]==(2*arr[i])) return true;
+               else if (arr[mid]>(2*arr[i]))  k = mid-1;
+               else j = mid+1;
+            }
+        }
+        return false;
+    }
+};
+int main()
+{
+    Solution obj;
+    vector<int> arr = {10,2,5,3};
+    bool result = obj.checkIfExist(arr);
+    if(result)
+        cout << "There exists an element and its double in the array." << endl;
+    else
+        cout << "No such elements found in the array." << endl;
+    return 0;
+}
