2b4e7c40836b8d50e28aaac2168b3bcfbee3c373
simranjitsit24-code <simranjits.it.24@nitj.ac.in>
2026-02-05 10:38:03 +0000

    Sort By Parity Bits

--- Vectors/SortbyParityII.cpp
+++ Vectors/SortbyParityII.cpp
@@ -2,8 +2,13 @@
 using namespace std;
 class Solution {
 public:
-    vector<int> sortArrayByParity(vector<int>& nums) {
-        deque<int> even;
+    vector<int> sortArrayByParityII(vector<int>& nums)
+    { 
+        int n = nums.size();
+        vector<int> ans2;
+        int i=0;
+        int j= n-1;
+         deque<int> even;
         vector<int> ans;
         for (int i: nums)
         {
@@ -11,24 +16,30 @@ public:
             {
              even.push_front(i);
             }
-            else  
+            else
             {
              even.push_back(i);
             }
         }
         for(int i:even)
         {
-          ans.push_back(i);
+          ans2.push_back(i);
         }
+        while (i<=j)
+        {
+        ans.push_back(ans2[i++]);
+        ans.push_back(ans2[j--]);
+        } 
         return ans;
     }
 };
+
 int main()
 {
     Solution sol;
-    vector<int> nums = {3,1,2,4};
-    vector<int> result = sol.sortArrayByParity(nums);
-    cout << "Array sorted by parity: ";
+    vector<int> nums = {4,2,5,7};
+    vector<int> result = sol.sortArrayByParityII(nums);
+    cout << "Array sorted by parity II: ";
     for (int num : result)
     {
         cout << num << " ";
